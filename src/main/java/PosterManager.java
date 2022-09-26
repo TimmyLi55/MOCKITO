c524432c7e71af041b312b524f187db1c69d050e
TimmyLi <artelaw@gmail.com>
2022-09-26 13:37:36 +0600

    Добавление тестов.

--- src/main/java/PosterManager.java
+++ src/main/java/PosterManager.java
@@ -2,24 +2,27 @@ public class PosterManager {
     private PosterData[] posters = new PosterData[0];
     private int countPostersByDef = 2;
 
-    public PosterManager(int count) {
-        countPostersByDef = count;
+    public PosterManager(int count) { // в случае когда в конструктор введут данные <=0 , беру кол-во постеров по умолчанию.
+        if (count <= 0) {
+            return;
+        } else {
+            countPostersByDef = count;
+        }
     }
-
     public PosterManager() {
 
-    }
-
-    public void savePoster(PosterData poster) {
-        PosterData[] tmp = new PosterData[posters.length + 1];
-        for (int i = 0; i < posters.length; i++) {
-            tmp[i] = posters[i];
         }
-        tmp[tmp.length - 1] = poster;
-        posters = tmp;
-    }
 
-    public PosterData[] findAll() {
+        public void savePoster (PosterData poster){
+            PosterData[] tmp = new PosterData[posters.length + 1];
+            for (int i = 0; i < posters.length; i++) {
+                tmp[i] = posters[i];
+            }
+            tmp[tmp.length - 1] = poster;
+            posters = tmp;
+        }
+
+        public PosterData[] findAll () {
 /*
         PosterData[] firstByDef = new PosterData[countPostersByDef];
         if (posters.length <= countPostersByDef) {
@@ -33,25 +36,25 @@ public class PosterManager {
         }*/
 
 
-    }
-
-    public PosterData[] findLast() {
-        if (posters.length <= countPostersByDef) {
-            PosterData[] lastByDef = new PosterData[posters.length];
-            int index = 0;
-            for (int i = 0; i < posters.length; i++) {
-                lastByDef[i] = posters[posters.length - index];
-                index++;
-            }
-            return  lastByDef;
-        } else {
-            PosterData[] lastByDef = new PosterData[countPostersByDef];
-            int index = 0;
-            for (int i = 0; i < countPostersByDef; i++) {
-                lastByDef[i] = posters[countPostersByDef - 1 - index];
-                index++;
-            }
-            return  lastByDef;
         }
-    }
+
+        public PosterData[] findLast () {
+            if (posters.length <= countPostersByDef) {
+                PosterData[] lastByDef = new PosterData[posters.length];
+                int index = 0;
+                for (int i = 0; i < posters.length; i++) {
+                    lastByDef[i] = posters[posters.length - index];
+                    index++;
+                }
+                return lastByDef;
+            } else {
+                PosterData[] lastByDef = new PosterData[countPostersByDef];
+                int index = 0;
+                for (int i = 0; i < countPostersByDef; i++) {
+                    lastByDef[i] = posters[countPostersByDef - 1 - index];
+                    index++;
+                }
+                return lastByDef;
+            }
+        }
     }
--- src/test/java/PosterManagerTest.java
+++ src/test/java/PosterManagerTest.java
@@ -7,20 +7,37 @@ public class PosterManagerTest {
     PosterData poster3 = new PosterData(5, "Омен", 2018);
     PosterData poster4 = new PosterData(6, "Послезавтра", 2010);
     PosterData poster5 = new PosterData(7, "Брат", 1999);
+    PosterData poster6 = new PosterData(14, "Побег из шоушенка", 2001);
+    PosterData poster7 = new PosterData(16, "Без чувств", 2005);
+    PosterData poster8 = new PosterData(22, "Амероиканский пирог", 2006);
+    PosterData poster9 = new PosterData(26, "Привет", 2015);
+    PosterData poster10 = new PosterData(27, "Брат-2", 2005);
 
     @Test
-    public void test() {
-        PosterManager manager = new PosterManager();
+    public void outputTheOnePoster() {
+        PosterManager manager = new PosterManager(1);
         manager.savePoster(poster1);
         manager.savePoster(poster2);
-        manager.savePoster(poster3);
-        manager.savePoster(poster4);
-        manager.savePoster(poster5);
 
-        PosterData[] expected = {poster2, poster1};
+
+        PosterData[] expected = {poster1};
         PosterData[] actual = manager.findLast();
 
-        Assertions.assertArrayEquals(expected,actual);
+        Assertions.assertArrayEquals(expected, actual);
 
     }
+
+    @Test
+    public void posterOutputWithZeroConstructorSetting() {
+        PosterManager manager = new PosterManager(0);
+        manager.savePoster(poster1);
+        manager.savePoster(poster2);
+
+        PosterData[] expected = {poster1, poster2};
+        PosterData[] actual = manager.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
 }
