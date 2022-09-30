2e0be35a653fbe332afa5f5646e2b24c7aa6a2c4
TimmyLi <artelaw@gmail.com>
2022-09-30 10:23:02 +0600

    Форматирование кода

--- src/main/java/PosterManager.java
+++ src/main/java/PosterManager.java
@@ -9,25 +9,26 @@ public class PosterManager {
             countPostersByDef = count;
         }
     }
+
     public PosterManager() {
 
-        }
+    }
 
-        public void savePoster (PosterData poster){
-            PosterData[] tmp = new PosterData[posters.length + 1];
-            for (int i = 0; i < posters.length; i++) {
-                tmp[i] = posters[i];
-            }
-            tmp[tmp.length - 1] = poster;
-            posters = tmp;
+    public void savePoster(PosterData poster) {
+        PosterData[] tmp = new PosterData[posters.length + 1];
+        for (int i = 0; i < posters.length; i++) {
+            tmp[i] = posters[i];
         }
+        tmp[tmp.length - 1] = poster;
+        posters = tmp;
+    }
 
-        public PosterData[] findAll () {
+    public PosterData[] findAll() {
 /*
         PosterData[] firstByDef = new PosterData[countPostersByDef];
         if (posters.length <= countPostersByDef) {
 */
-            return posters;
+        return posters;
 /*        } else {
             for (int i = 0; i < firstByDef.length; i++) {
                 firstByDef[i] = posters[i];
@@ -36,25 +37,25 @@ public class PosterManager {
         }*/
 
 
-        }
+    }
 
-        public PosterData[] findLast () {
-            if (posters.length <= countPostersByDef) {
-                PosterData[] lastByDef = new PosterData[posters.length];
-                int index = 0;
-                for (int i = 0; i < posters.length; i++) {
-                    lastByDef[i] = posters[posters.length - index];
-                    index++;
-                }
-                return lastByDef;
-            } else {
-                PosterData[] lastByDef = new PosterData[countPostersByDef];
-                int index = 0;
-                for (int i = 0; i < countPostersByDef; i++) {
-                    lastByDef[i] = posters[countPostersByDef - 1 - index];
-                    index++;
-                }
-                return lastByDef;
+    public PosterData[] findLast() {
+        if (posters.length <= countPostersByDef) {
+            PosterData[] lastByDef = new PosterData[posters.length];
+            int index = 0;
+            for (int i = 0; i < posters.length; i++) {
+                lastByDef[i] = posters[posters.length - index];
+                index++;
             }
+            return lastByDef;
+        } else {
+            PosterData[] lastByDef = new PosterData[countPostersByDef];
+            int index = 0;
+            for (int i = 0; i < countPostersByDef; i++) {
+                lastByDef[i] = posters[countPostersByDef - 1 - index];
+                index++;
+            }
+            return lastByDef;
         }
     }
+}
